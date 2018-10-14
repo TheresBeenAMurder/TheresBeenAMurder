0a0f927144c3e182e5ddd9442d1e8450a0f607f5
PIXEL\Inky <tinysquiddle@gmail.com>
2018-10-14 13:47:24 -0400

    Camera has post processing, viewfinder, way to identify items in the scene/name them outside of tags, and prints photo onto one model

---
+++ Assets/IdentifyItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdentifyItem : MonoBehaviour {
+
+	// Use this for initialization
+	public Text currentItemFound;
+	void Start () {
+		currentItemFound.text = "searching";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LookForItem();
+	}
+
+	void LookForItem(){
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		//Debug.DrawRay(transform.position, fwd, Color.green);
+		if (Physics.Raycast(transform.position, fwd, out hit, 100)) {
+			Debug.DrawRay(transform.position, fwd * hit.distance, Color.green);
+			ItemInfo itemInfo = hit.collider.gameObject.GetComponent<ItemInfo>();
+			Debug.Log("Found Item");
+            if(itemInfo != null){
+				currentItemFound.text = itemInfo.itemName;
+				Debug.Log("Looking at:"+itemInfo.itemName);
+			}
+			else{
+				currentItemFound.text = "searching";
+			}
+		}
+	}
+}
---
+++ Assets/TakePicture.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakePicture : MonoBehaviour {
+	public Camera imageCaptureCamera;
+	public bool takePic;
+	public Texture2D photo; 
+	public MeshRenderer photoModel;
+
+	// Use this for initialization
+	void Start () {
+		photo = new Texture2D(imageCaptureCamera.targetTexture.width, imageCaptureCamera.targetTexture.height);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(takePic){
+		photo = WriteRTImage(imageCaptureCamera);
+		photoModel.material.mainTexture = photo;
+		takePic = false; 
+		}
+	} 
+
+	 Texture2D WriteRTImage(Camera camera)
+    {
+        // The Render Texture in RenderTexture.active is the one
+        // that will be read by ReadPixels.
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = camera.targetTexture;
+
+        // Render the camera's view.
+        camera.Render();
+
+        // Make a new texture and read the active Render Texture into it.
+        Texture2D image = new Texture2D(camera.targetTexture.width, camera.targetTexture.height);
+        image.ReadPixels(new Rect(0, 0, camera.targetTexture.width, camera.targetTexture.height), 0, 0);
+        image.Apply();
+
+        // Replace the original active Render Texture.
+        RenderTexture.active = currentRT;
+        return image;
+    }
+}
