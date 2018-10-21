f7bd22ef55be76f32dcfe8a6458060bfdd0cf798
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2018-10-21 16:07:23 -0400

    neat piano demo scene

--- Assets/Scripts/PlayerPiano.cs
+++ Assets/Scripts/PlayerPiano.cs
@@ -9,9 +9,12 @@ public class PlayerPiano : MonoBehaviour {
 
     public MovableWall[] walls;
 
+    public PlayerPianoPaper test;
+
 	// Use this for initialization
 	void Start () {
         //fill the dictionary
+        movableWalls = new Dictionary<string, MovableWall>();
         movableWalls.Clear();
         foreach(MovableWall mw in walls)
         {
@@ -19,10 +22,46 @@ public class PlayerPiano : MonoBehaviour {
             movableWalls.Add(mw.pianoKey, mw);
 
         }
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Return))
+        {
+            insertPaper(test);
+
+        }
 	}
+
+    void insertPaper(PlayerPianoPaper inserted)
+    {
+       
+        //construct our string
+        string s = "";
+
+        for(int i = 0; i < inserted._nodeStatus.Length; i++)
+        {
+            if (inserted._nodeStatus[i])
+            {
+                s += "1";
+
+            }
+            else
+            {
+                s += "0";
+
+            }
+
+
+        }
+        //check if it's the key to any walls
+        if (movableWalls.ContainsKey(s))
+        {
+
+            movableWalls[s].moveWall();
+
+        }
+
+    }
 }
--- Assets/Scripts/PlayerPianoPaper.cs
+++ Assets/Scripts/PlayerPianoPaper.cs
@@ -19,12 +19,56 @@ public class PlayerPianoPaper : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            activePunch(0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            activePunch(1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            activePunch(2);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            activePunch(3);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            activePunch(4);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            activePunch(5);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            activePunch(6);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            activePunch(7);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            activePunch(8);
+
+        }
+    }
 
     public void punchMe(List<int> nodesToPunch)
     {
@@ -32,9 +76,9 @@ public class PlayerPianoPaper : MonoBehaviour {
     
         _nodeStatus =  new bool[] { false, false, false, false, false, false, false, false, false };
 
-        string[] _punchBools = new string[] { "Punch0", "Punch1", "Punch2", "Punch3", "Punch4", "Punch5", "Punch6", "Punch7", "Punch8", };
+        string[] _punchBools = new string[] { "_Punch0", "_Punch1", "_Punch2", "_Punch3", "_Punch4", "_Punch5", "_Punch6", "_Punch7", "_Punch8" };
 
-        foreach(string s in _punchBools)
+        foreach (string s in _punchBools)
         {
             _thisPaperMat.SetInt(s, 0);
         }
@@ -49,6 +93,27 @@ public class PlayerPianoPaper : MonoBehaviour {
 
         //put code here for swapping out materials
 
+
+    }
+
+    public void activePunch(int i)
+    {
+
+
+        string[] _punchBools = new string[] { "_Punch0", "_Punch1", "_Punch2", "_Punch3", "_Punch4", "_Punch5", "_Punch6", "_Punch7", "_Punch8" };
+
+        if (_nodeStatus[i])
+        {
+            _thisPaperMat.SetInt(_punchBools[i], 0);
+
+        }
+        else
+        {
+            _thisPaperMat.SetInt(_punchBools[i], 1);
+
+        }
+
+        _nodeStatus[i] = !_nodeStatus[i];
 
     }
 }
