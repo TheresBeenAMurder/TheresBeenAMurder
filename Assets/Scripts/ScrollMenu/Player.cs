9c0c20317ae8ddf35f7541116574f50ea5072393
SamaraPainter <35310274+SamaraPainter@users.noreply.github.com>
2018-10-25 20:43:40 -0400

    I have no idea what I'm doing, this is still in progress

---
+++ Assets/Scripts/ScrollMenu/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject murderBoardBase;
+    public OVRInput.Button murderBoardButton;
+    public GameObject playerHead;
+
+	private void Start ()
+    {
+        // make sure that the player can't see this upon start
+        murderBoardBase.GetComponent<Renderer>().enabled = false;
+        murderBoardBase.SetActive(false);
+	}
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(murderBoardButton))
+        {
+            if (murderBoardBase.activeSelf)
+            {
+                // turn it off
+                murderBoardBase.GetComponent<Renderer>().enabled = false;
+                murderBoardBase.SetActive(false);
+            }
+            else
+            {
+                // turn it on
+                murderBoardBase.SetActive(true);
+                Vector3 newPos = new Vector3(playerHead.transform.position.x,
+                    playerHead.transform.position.y,
+                    playerHead.transform.position.z + .75f);
+                murderBoardBase.GetComponent<Rigidbody>().MovePosition(newPos);
+                murderBoardBase.GetComponent<Renderer>().enabled = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScrollMenu/PlayerPoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPoint : MonoBehaviour {
+
+    public GameObject murderBoard;
+    public GameObject rHand;
+
+    private bool canPoint = false;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (murderBoard.activeSelf)
+        {
+            // turn on ray checking
+            canPoint = true;
+        }
+        else
+        {
+            // turn off ray checking
+            canPoint = false;
+        }
+
+        if (canPoint)
+        {
+            // check if player is pointing
+            if (OVRInput.Get(OVRInput.RawButton.RHandTrigger) &&
+                OVRInput.Get(OVRInput.RawTouch.RThumbRest) &&
+                !OVRInput.Get(OVRInput.RawTouch.RIndexTrigger))
+            {
+                Debug.Log("Player is pointing");
+                // throw out a ray and see if it hits the canvas
+                Ray ray = new Ray(rHand.transform.position, rHand.transform.forward);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.tag == "MurderBoardScroll")
+                    {
+                        Debug.Log("Player is pointing at scroll");
+                    }
+                }
+            }
+        }
+	}
+}
