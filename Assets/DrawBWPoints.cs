ae655e819b8d68812624127bce056bbeba9054e3
DESKTOP-EK807HG\Chris <tinysquiddle@gmail.com>
2018-10-29 14:07:34 -0400

    Line Render scene Draw Connections has a board with notes that get lines drawn between them on play

---
+++ Assets/DrawBWPoints.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBWPoints : MonoBehaviour {
+	private LineRenderer lineRenderer; 
+	private float counter; 
+	private float distance; 
+
+	public float drawSpeed = 5f; 
+	public Transform pointA; 
+	public Transform pointB; 
+	public Transform pointC; 
+	public Transform pointD;
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.SetPosition(0, pointA.position);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lineRenderer.SetPosition(1, pointB.position);
+		lineRenderer.SetPosition(2, pointC.position);
+		lineRenderer.SetPosition(3, pointD.position);
+		//lineRenderer.SetPosition(1, pointB.position);
+	}
+}
