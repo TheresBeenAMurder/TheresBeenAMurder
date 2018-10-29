0c8adc8f5617b10d4072744496e37c716c453993
SamaraPainter <35310274+SamaraPainter@users.noreply.github.com>
2018-10-28 23:26:30 -0400

    you can place photo prefabs on board and they stay there until you remove them, still buggy but main functionality is there

--- Assets/Scripts/ScrollMenu/MurderBoard.cs
+++ Assets/Scripts/ScrollMenu/MurderBoard.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MurderBoard : MonoBehaviour {
 
-    
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name);
-
         if (other.gameObject.tag == "BoardPhoto")
         {
-            Debug.Log("A photo is touching me");
+            // reparent photo to board
+            other.gameObject.transform.parent = transform;
+            other.gameObject.transform.localScale = new Vector3(.15f, .25f, .005f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "BoardPhoto")
+        {
+            other.gameObject.transform.parent = null;
+            other.gameObject.transform.localScale = new Vector3(.108f, .105f, .004f);
         }
     }
 }
