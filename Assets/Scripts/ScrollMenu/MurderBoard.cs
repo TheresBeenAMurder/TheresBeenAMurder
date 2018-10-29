d02756e2edb9e8d680a17a7a0d2fea4bc910103a
SamaraPainter <35310274+SamaraPainter@users.noreply.github.com>
2018-10-28 20:15:21 -0400

    getting ern access to gestures and photo prefab

---
+++ Assets/Scripts/ScrollMenu/MurderBoard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MurderBoard : MonoBehaviour {
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+
+        if (other.gameObject.tag == "BoardPhoto")
+        {
+            Debug.Log("A photo is touching me");
+        }
+    }
+}
