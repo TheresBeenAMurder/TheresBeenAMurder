ea6bc761d051a055786497068f09d5a13fcab8cd
SamaraPainter <35310274+SamaraPainter@users.noreply.github.com>
2018-11-04 22:13:18 -0500

    adding plant puzzle prototype, stick the labels in the things to do the thing

---
+++ Assets/Scripts/PlantPuzzle/PuzzleSolution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PuzzleSolution : MonoBehaviour {
+
+    public float moveTime = 1f / .1f;
+
+    private bool isSolved = false;
+
+    public bool IsSolved()
+    {
+        return isSolved;
+    }
+
+	public void PuzzleSolve()
+    {
+        StartCoroutine("RevealPainting");
+        isSolved = true;
+    }
+
+    // Moves painting cover so painting is now visible
+    public IEnumerator RevealPainting()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // Slide cover to left of painting
+        Vector3 newPos = new Vector3(transform.parent.position.x + 1,
+            transform.position.y,
+            transform.position.z);
+
+        yield return StartCoroutine(Movement.SmoothMove(newPos, moveTime, rigidbody));
+    }
+}
