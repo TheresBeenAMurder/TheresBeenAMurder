409e4b14a7281558e3cc08dad0d7b89cc87999d7
Erin's Laptop <nern.strudel@gmail.com>
2018-11-10 04:09:26 -0500

    deet

---
+++ Assets/Scripts/PianoPuzzle/BottomCylinder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomCylinder : MonoBehaviour {
+
+
+    int cylinderCount =1;
+    BoxCollider collider;
+
+    public float cylinderHeight;
+
+	// Use this for initialization
+	void Start () {
+        cylinderCount = GetComponentsInChildren<PianoCylinder>().Length + 1;
+        gameObject.AddComponent<BoxCollider>();
+        collider = GetComponent<BoxCollider>();
+        resizeBoxCollider();
+
+    }
+
+    public void addCylinder()
+    {
+        cylinderCount += 1;
+        resizeBoxCollider();
+
+    }
+
+    public void removeCylinder()
+    {
+
+        cylinderCount -= 1;
+        resizeBoxCollider();
+    }
+	
+    public void resizeBoxCollider()
+    {
+
+        collider.size = new Vector3(collider.size.x, cylinderCount * cylinderHeight, collider.size.z);
+
+    }
+
+}
---
+++ Assets/Scripts/PianoPuzzle/CylinderVending.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderVending : MonoBehaviour {
+
+    public GameObject[] cylinderPrefabs;
+
+    public Transform outputLocation;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void vendCylinder(int colorToVend)
+    {
+        GameObject vendedCylinder = Instantiate(cylinderPrefabs[colorToVend]);
+        vendedCylinder.transform.position = outputLocation.position;
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
