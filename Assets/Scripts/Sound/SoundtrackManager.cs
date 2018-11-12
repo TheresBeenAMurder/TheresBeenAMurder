7fd4375b214dd22cd3b0e6f492e26cb0b6bfd58e
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2018-11-12 14:32:23 -0500

    the soundzzzzzzz. also, polaroid

---
+++ Assets/Scripts/Sound/SoundtrackManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackManager : MonoBehaviour {
+
+    //hnngn
+    public SoundtrackLayer[] soundtrackLayers;
+
+
+    //currently set to work with one track, will add functionlity to switch tracks
+
+
+	// Use this for initialization
+	void Start () {
+		foreach(SoundtrackLayer audio in soundtrackLayers)
+        {
+            audio.GetComponent<AudioSource>().volume = .6f;
+
+
+        }
+	}
+	
+    public void switchTrack(int track)
+    {
+
+        foreach(SoundtrackLayer sl in soundtrackLayers)
+        {
+
+            sl.switchTrack(track);
+
+        }
+
+    }
+
+}
