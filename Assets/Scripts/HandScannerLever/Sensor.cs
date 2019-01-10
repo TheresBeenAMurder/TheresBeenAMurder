25d9a86177d71168c3469dee590d2f042b63ac3f
SamaraPainter <35310274+SamaraPainter@users.noreply.github.com>
2019-01-10 13:03:09 -0500

    initial addition of hand scanner/lever prototype - levers are stupid

---
+++ Assets/Scripts/HandScannerLever/Sensor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    public Material correctColor;
+    public int correctKeyID;
+    public Material incorrectColor;
+
+    private Key currentKey;
+    private Material originalColor;
+
+    private void ChangeColor(Material newColor)
+    {
+        gameObject.GetComponent<Renderer>().material = newColor;
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        Key key = collision.collider.gameObject.GetComponent<Key>();
+
+        // Only change color when key exits, nothing else
+        if (key != null && key == currentKey)
+        {
+            ChangeColor(originalColor);
+            currentKey = null;
+        }
+    }
+
+    public void OnCollisionStay(Collision collision)
+    {
+        Key key = collision.collider.gameObject.GetComponent<Key>();
+        if (key != null && currentKey == null)
+        {
+            currentKey = key;
+            if (key.ID == correctKeyID)
+            {
+                //  PUZZLE SOLVED HERE, DO THE OTHER STUFF
+                ChangeColor(correctColor);
+            }
+            else
+            {
+                ChangeColor(incorrectColor);
+            }
+        }
+    }
+
+    public void Start()
+    {
+        originalColor = gameObject.GetComponent<Renderer>().material;
+    }
+}
