0f7a4742da9830b1800c589e0a91460b75696ebf
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2019-01-21 14:49:12 -0500

    better cylinders yay

--- Assets/Scripts/Puzzles/4Piano/StickyComponent.cs
+++ Assets/Scripts/Puzzles/4Piano/StickyComponent.cs
@@ -5,22 +5,30 @@ public class StickyComponent : MonoBehaviour
     public PianoCylinder parent;
     public PianoCylinderManager parentManager;
     public Transform snapPoint;
-    public float tolerance = .1f;
+    public float tolerance = .5f;
     public int topOrBottomIndex;
+    public GameObject attachedCyl;
+    public int attachedIndex;
+
+    private void Start()
+    {
+        attachedCyl = null;
+    }
+
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("StickyComponent"))
-        {
-            if (parentManager.isRotating)
-            {
-                parent.RemoveAttachedCylinder(topOrBottomIndex, other.GetComponent<StickyComponent>().topOrBottomIndex);
-            }
-            else if(parent.attached[topOrBottomIndex] != null)
-            {
-             //   other.gameObject.GetComponent<StickyComponent>().parent.transform.position = snapPoint.position;
-            }
-        }
+        //if (other.gameObject.CompareTag("StickyComponent"))
+        //{
+        //    if (parentManager.isRotating && parent.attached != null && parent.attached.Length > 0 && parent.attached[topOrBottomIndex] != null)
+        //    {
+        //        parent.RemoveAttachedCylinder(topOrBottomIndex, other.GetComponent<StickyComponent>().topOrBottomIndex);
+        //    }
+        //    else if(parent.attached[topOrBottomIndex] != null)
+        //    {
+        //     //   other.gameObject.GetComponent<StickyComponent>().parent.transform.position = snapPoint.position;
+        //    }
+        //}
     }
 
     private void OnTriggerStay(Collider other)
@@ -34,13 +42,24 @@ public class StickyComponent : MonoBehaviour
                 parent.AttachCylinder(other.gameObject.GetComponent<StickyComponent>().parent.gameObject,
                     topOrBottomIndex,
                     other.GetComponent<StickyComponent>().topOrBottomIndex);
-
-                if (Vector3.Distance(other.gameObject.GetComponent<StickyComponent>().parent.transform.position,
-                    snapPoint.position) > tolerance)
-                {
-                    other.gameObject.GetComponent<StickyComponent>().parent.transform.position = snapPoint.position;
-                }
+                attachedIndex = other.GetComponent<StickyComponent>().topOrBottomIndex;
+                attachedCyl = other.gameObject.GetComponent<StickyComponent>().parent.gameObject;
               }
+        }
+    }
+
+    private void Update()
+    {
+        if (attachedCyl != null)
+        {
+
+            if((Vector3.Distance(transform.position, attachedCyl.transform.position) > tolerance) && parentManager.isRotating)
+            {
+                attachedCyl = null;
+                parent.RemoveAttachedCylinder(topOrBottomIndex, attachedIndex);
+                
+            }
+
         }
     }
 }
