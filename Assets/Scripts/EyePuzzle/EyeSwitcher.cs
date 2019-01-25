d9432c233cb0b9a9216df6b3942bfeac6541d465
SamaraPainter <35310274+SamaraPainter@users.noreply.github.com>
2019-01-25 16:19:56 -0500

    adding proof of concept for the eye puzzle

---
+++ Assets/Scripts/EyePuzzle/EyeSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EyeSwitcher : MonoBehaviour
+{
+    public Camera eyeToSwitch;
+
+    private int oldCameraLayers;
+    private int everythingCameraLayer = -1;
+
+	public void Start ()
+    {
+        // Store old mask (Everything - OnlyVisibleToEye)
+        oldCameraLayers = eyeToSwitch.cullingMask;
+	}
+	
+    public void SwitchEye()
+    {
+        if (eyeToSwitch.cullingMask == oldCameraLayers)
+        {
+            eyeToSwitch.cullingMask = everythingCameraLayer;
+        }
+        else
+        {
+            eyeToSwitch.cullingMask = oldCameraLayers;
+        }
+    }
+	
+	public void Update ()
+    {
+		if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            SwitchEye();
+        }
+	}
+}
