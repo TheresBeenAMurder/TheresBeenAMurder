8141c6dfefaca23c4ef0b373ce0e801f1f6ad25a
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2019-02-11 11:16:51 -0500

    lots of goodies! sound is still broken but gallery puzzle hands, new desk lamp, creaky bois

---
+++ Assets/Scripts/Puzzles/2BioWall/BioWallTablet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BioWallTablet : OVRGrabbable {
+
+    public BioWallScreen screen;
+
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+        screen.grabbingHand = hand.gameObject;
+    }
+}
