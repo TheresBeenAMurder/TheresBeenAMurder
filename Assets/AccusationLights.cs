3e7c4c6438d5362edd8e7a9be074950686eeab93
Erin's Laptop <nern.strudel@gmail.com>
2019-03-03 20:01:33 -0500

    hello, i am the dumb

---
+++ Assets/AccusationLights.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccusationLights : MonoBehaviour {
+
+    public GameObject lights;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            if (lights.activeSelf)
+            {
+                turnOff();
+            }
+            else
+            {
+                turnRed();
+            }
+        }
+    }
+
+    void turnRed()
+    {
+        lights.SetActive(true);
+    }
+
+    void turnOff()
+    {
+        lights.SetActive(false);
+
+    }
+}
