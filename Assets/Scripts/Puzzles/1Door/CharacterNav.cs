ba23ac84bdd8e4bd31bb80752700f5891b8e7953
Erin's Laptop <nern.strudel@gmail.com>
2019-03-10 14:59:13 -0400

    NPCS walk to new position except they ZOOOOOOM

--- Assets/Scripts/Puzzles/1Door/CharacterNav.cs
+++ Assets/Scripts/Puzzles/1Door/CharacterNav.cs
@@ -7,9 +7,33 @@ public class CharacterNav : MonoBehaviour
 
     private NavMeshAgent agent;
 
+    public NPCAnimator animator;
+
+    bool walkStarted = false;
+    public float distance = 0.1f;
+
     public void Move()
     {
         agent.SetDestination(endPos.position);
+        animator.changeState(NPCAnimator.CHARACTERSTATE.WALKFORWARD);
+        walkStarted = true;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            Move();
+        }
+
+        if(walkStarted)
+        {
+            if(Vector3.Distance(transform.position, endPos.position) < distance)
+            {
+                walkStarted = false;
+                animator.changeState(NPCAnimator.CHARACTERSTATE.IDLE);
+            }
+        }
     }
 
     void Start ()
