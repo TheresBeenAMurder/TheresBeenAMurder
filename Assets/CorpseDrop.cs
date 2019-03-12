2e0c95bab2134fcfb9f12321de6d4e4356d9d60f
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2019-03-12 08:14:29 -0400

    LAST PUSH FOR WINTER TERM BOIS

--- Assets/CorpseDrop.cs
+++ Assets/CorpseDrop.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CorpseDrop : MonoBehaviour {
 
+    public SoundtrackManager sm;
     public GameObject corpse;
 
 	public void Drop()
     {
         corpse.SetActive(true);
+        sm.nextTrack();
     }
 }
