6dc25361643677d8dc1c8c4e41eba2c5c86a948b
Jo Brown <joebobjrjr@gmail.com>
2019-03-12 03:10:37 -0400

    Main Menu Screen
    And stuff to support it

---
+++ Assets/Scripts/UI/FXEnable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXEnable : MonoBehaviour {
+
+    public GameObject objectToDisable;
+    public static bool disabled = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (disabled)
+            objectToDisable.SetActive(false);
+        else
+            objectToDisable.SetActive(true);
+	}
+}
