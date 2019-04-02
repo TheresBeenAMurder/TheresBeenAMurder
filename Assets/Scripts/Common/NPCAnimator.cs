a64cbcbddedc71cd9ae07a2a8e641f29389c45d4
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2019-04-02 12:50:46 -0400

    erin is dumb

--- Assets/Scripts/Common/NPCAnimator.cs
+++ Assets/Scripts/Common/NPCAnimator.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 using UnityEngine;
 
 public class NPCAnimator : MonoBehaviour {
